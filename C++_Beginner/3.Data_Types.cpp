c112505d9a4feec56568ae7454d11f2ac65658ae
andy <jhyeonc@gmail.com>
2022-06-19 19:46:07 +0900

    first time learning cpp

---
+++ C++_Beginner/3.Data_Types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char grade  = 'A';
+    string phrase = "C++ is fun";
+    int age = 50;
+    double gpa = 2.3; // float is also available
+    bool isMale = true;
+
+    return 0;
+}
