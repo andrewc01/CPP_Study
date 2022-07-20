56c117a2b317eabf20cfc6130f02317de6ec0889
andy <jhyeonc@gmail.com>
2022-07-21 00:57:19 +0900

    Jul 20

---
+++ C++_Beginner_to_Advanced/1.Comments.cpp
@@ -0,0 +1,17 @@
+// This brings in the iostream library
+#include <iostream>
+
+int main(int argc, char **argc) {
+    // This is going to print "Hello World" to the console
+   std::cout << "Hello World in C++" << std::endl;
+
+   // This is one line comment
+
+   /*
+    This is a block comment that englobes multiple
+    lines of text
+   */
+   
+   return 0;
+   // Program ends here
+}
